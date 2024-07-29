1998d4b720237df680953322ac588be96e6cee95
Jonatan <costalesjonatan@gmail.com>
2024-07-29 06:31:40 -0300

    Added product details component and routing configuration.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
+import { ProductDetailsComponent } from './component/product-details/product-details.component';
 
-export const routes: Routes = [{
-  path: '',
-  component: HomeComponent,
-  title: 'Home page',
-},];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home page',
+  },
+  {
+    path: 'details',
+    component: ProductDetailsComponent,
+    title: 'Home d',
+  },
+];
--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, Inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from "@angular/common";
 import { ProductComponent } from './../product/product.component';
 import { ProductService } from '../../service/product.service';
